e98e9e8d79919af58bbb24f569a6fbcbda11dd0f
AnnaVilalievna <azaychenko1992@gmail.com>
2021-08-05 15:43:56 +0300

    js_lesson_2.template3.8

--- js_lesson_3/main.js
+++ js_lesson_3/main.js
@@ -27,7 +27,7 @@ function getMinAndLogMax(...args) {
     let minValue = args[0];
     let maxValue = args[0];
 
-    for (item of args) {
+    for (let item of args) {
         if (item < minValue) {
             minValue = item
         }
@@ -41,16 +41,73 @@ function getMinAndLogMax(...args) {
     return minValue;
 }
 
-let itfuy = getMinAndLogMax(5, 3, 24, 65, 6, 7);
+let minValue = getMinAndLogMax(5, 3, 24, 65, 6, 7);
 console.log(
-    'Min value from array', itfuy
+    'Min value from array', minValue
 )
 
 //5
-function blocktext(text){
-   document.write(`<div>${text}</div>`)
+function block_text(text) {
+    document.write(`<div>${text}</div>`)
 }
-blocktext(`text`);
+
+block_text(`text`);
 
 //6
+function block_ul(text) {
+    document.write(`<ul>
+<li>${text}</li>
+<li>${text}</li>
+<li>${text}</li>
+</ul>`)
+}
+
+block_ul(`Text2`);
+
+// 7
+function block_ul_loop(text, n) {
+    document.write(`<ul>`);
+    for (let step = 0; step < n; step++) {
+        document.write(`<li>${text}</li>`)
+    }
+    document.write(`</ul>`);
+}
+block_ul_loop(`text`,5);
+
+//8
+
+function buildList(array) {
+    document.write(`<ul>`);
+
+    for(let step = 0; step < array.length; step++) {
+        document.write(`<li>${array[step]}</li>`)
+    }
+
+    // array.forEach(el => document.write(`<li>${el}</li>`));
+
+    document.write(`</ul>`);
+}
+buildList([65123, 2424,'ery', 'eyerghe', true, false, true])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
